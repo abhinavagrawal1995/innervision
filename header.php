55b7bee7d023881a3dbe4bff8b6ef1dfee779692
abhinavagrawal1995 <abhinavagrawal1995@gmail.com>
2016-04-26 22:26:44 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,27 @@
+<h1><a href="index.php">Rakhi Imaging Centre</a></h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li>
+								<a href="#" class="icon fa-angle-down">Ultrasound scans</a>
+								<ul>
+									<li><a href="/viability-scan">Viability scan</a></li>
+									<li><a href="/nuchal-scan">Nuchal scan</a></li>
+									<li><a href="/anomaly-scan">Anomaly scan</a></li>
+									<li><a href="/cardiac-scan">Cardiac scan</a></li>
+									<li><a href="/cervical-scan">Cervical scan</a></li>
+									<li><a href="/wellbeing-scan">Wellbeing scan</a></li>									
+								</ul>
+							</li>
+							<li><a href="#">Harmony test</a></li>
+							<li>
+								<a href="#" class="icon fa-angle-down">Invasive test</a>
+								<ul>
+									<li><a href="/chorion-villus-sampling">Chorion villus sampling</a></li>
+									<li><a href="/amniocentesis">Amniocentesis</a></li>								
+								</ul>
+							</li>
+							<li><a href="#">Treatment Fees</a></li>
+							<li><a href="#" class="button">Contact Us</a></li>
+						</ul>
+					</nav>
